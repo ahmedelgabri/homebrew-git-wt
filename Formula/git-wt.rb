5f4e4f6cff87b2b3b06839f7c1dde076d60d720a
Ahmed El Gabri <ahmed@gabri.me>
2026-02-11 21:15:08 +0100

    Bump version from 0.4.0 to 0.5.1

--- Formula/git-wt.rb
+++ Formula/git-wt.rb
@@ -3,7 +3,7 @@ class GitWt < Formula
   homepage "https://github.com/ahmedelgabri/git-wt"
   url "https://github.com/ahmedelgabri/git-wt.git",
       branch: "main"
-  version "0.4.0"
+  version "0.5.1"
   license "MIT"
   head "https://github.com/ahmedelgabri/git-wt.git", branch: "main"
 
